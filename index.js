749588ad91d4c25a4a9af83e375d7addfe7d767f
Mohammad Anwari <mdamt@mdamt.net>
2015-10-08 23:08:41 +0700

    Winning bidders

--- index.js
+++ index.js
@@ -23,13 +23,84 @@ EprocScraper.prototype.lelang = function() {
         var stage = data.find('.tahap > a.jpopup').text();
         var hps = data.find('.pkt_hps > span').text();
         var link = data.find('.pkt_nama > b > a.jpopup').attr('href');;
+        var id = link.substr(link.lastIndexOf('/') + 1);
 
         var entry = {
           title: title,
           agency: agency,
           stage: stage,
           hps: hps,
-          link: link
+          link: link,
+          id: id
+        }
+
+        packages.push(entry);
+      });
+      return resolve(packages);
+    });
+  });
+}
+
+EprocScraper.prototype.lelangUmum = function() {
+  var self = this;
+  return new Promise(function(resolve, reject) {
+    request(self.url + '/publiclelangumum', function(err, response, html) {
+      if (err) return reject(err);
+
+      var $ = cheerio.load(html);
+
+      var packages = [];
+      $('.horizLine').filter(function() {
+        var data = $(this);
+
+        var title = data.find('.plp_nama_paket > a.jpopup').text();
+        var agency = data.find('.stk_nama').text();
+        var doc = data.find('.ambil_dok').text();
+        var hps = data.find('.plp_hps > div').text();
+        var link = data.find('.plp_nama_paket > a.jpopup').attr('href');;
+        var id = link.substr(link.lastIndexOf('/') + 1);
+
+        var entry = {
+          title: title,
+          agency: agency,
+          doc: doc,
+          hps: hps,
+          link: link,
+          id: id
+        }
+
+        packages.push(entry);
+      });
+      return resolve(packages);
+    });
+  });
+}
+
+EprocScraper.prototype.pemenang = function() {
+  var self = this;
+  return new Promise(function(resolve, reject) {
+    request(self.url + '/lelang/pemenangcari', function(err, response, html) {
+      if (err) return reject(err);
+
+      var $ = cheerio.load(html);
+
+      var packages = [];
+      $('.horizLineTop').filter(function() {
+        var data = $(this);
+
+        var title = data.find('.pkt_nama > b > a.jpopup').text();
+        var agency = data.find('.agc_nama').text();
+        var hps = data.find('.pkt_hps > span').text();
+        var link = data.find('.pkt_nama > b > a.jpopup').attr('href');;
+        var id = link.substr(link.lastIndexOf('/') + 1);
+
+ 
+        var entry = {
+          title: title,
+          agency: agency,
+          hps: hps,
+          link: link,
+          id: id
         }
 
         packages.push(entry);
--- test/index.js
+++ test/index.js
@@ -4,15 +4,53 @@ var EprocScraper = require('../index');
 var nock = require('nock');
 
 describe('e-Proc Scraper', function() {
-  var scope = nock(url)
+  nock(url)
     .get('/eproc/lelang')
     .replyWithFile(200, __dirname + '/assets/lelang-1.html');
+
+  nock(url)
+    .get('/eproc/publiclelangumum')
+    .replyWithFile(200, __dirname + '/assets/lelang-2.html');
+
+  nock(url)
+    .get('/eproc/lelang/pemenangcari')
+    .replyWithFile(200, __dirname + '/assets/lelang-3.html');
 
   it('should return packages', function(done) {
     var L = new EprocScraper(url + '/eproc');
     L.lelang().then(function(packages) {
       packages.length.should.equal(50);
+      for (var i = 0; i < packages.length; i++) {
+        var p = packages[i];
+        p.link.indexOf(p.id).should.equal(p.link.length - p.id.length);
+      }
       done();
     });
   });
+
+  it('should return packages for "lelang umum non-eproc"', function(done) {
+    var L = new EprocScraper(url + '/eproc');
+    L.lelangUmum().then(function(packages) {
+      packages.length.should.equal(50);
+      for (var i = 0; i < packages.length; i++) {
+        var p = packages[i];
+        p.link.indexOf(p.id).should.equal(p.link.length - p.id.length);
+      }
+      done();
+    });
+  });
+
+  it('should return the winning bidders', function(done) {
+    var L = new EprocScraper(url + '/eproc');
+    L.pemenang().then(function(winners) {
+      winners.length.should.equal(30);
+      for (var i = 0; i < winners.length; i++) {
+        var p = winners[i];
+        p.link.indexOf(p.id).should.equal(p.link.length - p.id.length);
+      }
+      done();
+    });
+  });
+
+
 });
